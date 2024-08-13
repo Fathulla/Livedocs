9f2c5d9f85b4c41b759306acf43d37bfff10307e
Fathulla Rashidov <fathullarasidov239@gmail.com>
2024-08-13 15:14:08 +0500

    added the title change function on and strted to do some realtime features

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -9,10 +10,15 @@ import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
-      <ClientSideSuspense fallback={<Loader/>}>
-        {children}
-      </ClientSideSuspense> 
+    <LiveblocksProvider
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users
+      }}
+      authEndpoint={"/api/liveblocks-auth"}
+    >
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
